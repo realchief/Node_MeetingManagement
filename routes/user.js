9d16ecfa1645537043626f2bc24706b1a384b2d4
realchief <stryokka@gmail.com>
2018-10-19 14:30:36 -0400

    built forgot page

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,8 @@ const moment = require("moment-timezone");
 var userInfo = require('../controllers/users')
 
 var utilities = require('../controllers/utilities')
-
+var crypto = require('crypto');
+var nodemailer = require('nodemailer');
 var Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
@@ -174,6 +175,67 @@ router.get('/signup', function(req, res, next) {
         });
         
     }
+});
+
+router.get('/forgot', function(req, res) {
+
+    res.render('forgot', {
+        layout: 'main',
+        
+    });
+    
+});
+
+router.post('/forgot', function(req, res, next) {
+
+    async.waterfall([
+        function(done) {
+            crypto.randomBytes(20, function(err, buf) {
+                var token = buf.toString('hex');
+                done(err, token);
+            });
+        },
+        function(token, done) {
+            User.findOne({ email: req.body.email }, function(err, user) {
+                if (!user) {
+                    req.flash('error', 'No account with that email address exists.');
+                    return res.redirect('/forgot');
+                }
+        
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+        
+                user.save(function(err) {
+                    done(err, token, user);
+                });
+            });
+        },
+        function(token, user, done) {
+            var smtpTransport = nodemailer.createTransport('SMTP', {
+                service: 'SendGrid',
+                auth: {
+                    user: '!!! YOUR SENDGRID USERNAME !!!',
+                    pass: '!!! YOUR SENDGRID PASSWORD !!!'
+                }
+            });
+            var mailOptions = {
+                to: user.email,
+                from: 'passwordreset@demo.com',
+                subject: 'Node.js Password Reset',
+                text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+            };
+            smtpTransport.sendMail(mailOptions, function(err) {
+                req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+                done(err, 'done');
+            });
+        }
+    ], function(err) {
+        if (err) return next(err);
+        res.redirect('/forgot');
+    });
 });
 
 router.post('/signup', function(req, res) {
